d27aacb18a1fc98fac3628360d5ea0decf7fef99
Bukgeuk_ <BukgeukTV@gmail.com>
2020-04-08 22:29:54 +0900

    reduce image

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function init_background(){
-    const images = ['images/background1.png', 'images/background2.png', 'images/background3.png'];
+    const images = ['images/background1.jpg', 'images/background2.jpg', 'images/background3.jpg'];
     document.body.style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
 }
 
